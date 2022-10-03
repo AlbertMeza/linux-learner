d974bcf7b5b881b0534c939548af02fde4826bce
AlbertMeza <almez5202@gmail.com>
2022-10-03 11:14:43 -0700

    Initial Commit

---
+++ src/main/java/Commands.java
@@ -0,0 +1,34 @@
+public enum Commands {
+  CHANGE_DIRECTORY("cd"),
+  CLEAR("clear"),
+  EXIT("exit"),
+  LIST_DIRECTORIES("ls"),
+  PRINT_WORKING_DIRECTORY("pwd"),
+  TOUCH("touch");
+
+
+
+  private final String command;
+
+  Commands(String command){
+    this.command = command;
+  }
+
+  public String toString() {
+    String name = name().toLowerCase();
+    String printedName = ("" + name.charAt(0)).toUpperCase();
+    for(int i = 1; i < name.length(); i++){
+      if(name.charAt(i) == '_') {
+        printedName += ' ';
+      }
+      else {
+        printedName += name.charAt(i);
+      }
+    }
+    return printedName;
+  }
+
+  public String command() {
+    return command;
+  }
+}
---
+++ src/main/java/StringLiterals.java
@@ -0,0 +1,4 @@
+public class StringLiterals {
+  public final String EXIT =("Saving session...\n...copying shared history..\n...saving history...truncating history files...\n...completed.\n\n[Process completed]");
+
+}
