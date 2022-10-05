5ab2b2efbff0e31254687c06b8e979fe4042c461
gmejia3 <gemejiajr@gmail.com>
2022-10-05 14:33:10 -0700

    Moving around the packages and UserTerminal interface.

---
+++ src/main/java/com/linux/Main.java
@@ -0,0 +1,14 @@
+package com.linux;
+
+import com.linux.controller.Terminal;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      Terminal terminal = new Terminal();
+      terminal.startTerminal();
+
+  }
+
+}
