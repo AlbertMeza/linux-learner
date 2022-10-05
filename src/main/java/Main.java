2e903bdc64d091be6a11bd43cd10638ecbde2ee2
AlbertMeza <almez5202@gmail.com>
2022-10-05 12:31:55 -0700

    Terminal small change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@
 public class Main {
 
   public static void main(String[] args) {
-      Terminal terminal = new Terminal();
-      terminal.startTerminal();
+      new Terminal().startTerminal();
   }
 }
